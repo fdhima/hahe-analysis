47625e41aa07202568653760906372eec67759a5
dflorian251 <dflorian251@gmail.com>
2025-11-01 13:07:12 +0200

    #2 Scrape alla available data.

--- scraper.py
+++ scraper.py
@@ -6,6 +6,57 @@ import os
 import re
 
 
+def save_to_csv(data: list[dict], filename: str):
+    """Save the list of dictionaries to a CSV file.
+    
+    Args:
+        data (list): List of dictionaries containing the scraped data.
+        filename (str): The name of the CSV file to save the data.
+    """
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
+
+def scrape_data(soup: BeautifulSoup):
+    """Scrape data from the BeautifulSoup object and return a list of dictionaries.
+    
+    Args:
+        soup (BeautifulSoup): The BeautifulSoup object containing the HTML content.
+    """
+    data = []
+    soup_data = soup.find_all('div', class_='stats-item')
+    for item in soup_data:
+        institution = item  .find('div', class_='stats-item-title').text.strip()
+        institution = re.search("^ΙΔΡΥΜΑ ::", institution)
+        
+        established = item.find('span', class_='stats-item-date').text.strip()
+        established = re.search("Ημ/νία Ίδρυσης: ", established)
+        
+        variables = item.find_all('div', class_='stats-item-variable-title')
+        print(f"variables: {variables}")
+        for var in variables:
+            print(var)
+            variable = var.text.strip()
+            if variable == "Απόφοιτοι ΠΠΣ Ιδρύματος":
+                graduates = var.find_next_sibling('div', class_='stats-item-variable-value').text.strip()
+            elif variable == "Εγγεγραμμένοι φοιτητές ΠΠΣ Ιδρύματος":
+                registed_students = var.find_next_sibling('div', class_='stats-item-variable-value').text.strip()
+            elif variable == "Εισαχθέντες φοιτητές ΠΠΣ Ιδρύματος":
+                freshmen = var.find_next_sibling('div', class_='stats-item-variable-value').text.strip()
+            elif variable == "Ενεργοί φοιτητές ΠΠΣ Ιδρύματος":
+                active_students = var.find_next_sibling('div', class_='stats-item-variable-value').text.strip()
+
+        data.append({
+            'institution': institution.string[institution.end():],
+            'established': established.string[established.end():],
+            "gradutes": graduates,
+            "registed_students": registed_students,
+            "freshmen": freshmen,
+            "active_students": active_students,
+        })
+
+        save_to_csv(data, f'hahe_{payload.get("filter[collectionyear]")}.csv')
+
+
 load_dotenv()
 DATA_SOURCE = os.getenv("DATA_SOURCE")
 
@@ -13,46 +64,14 @@ payload = {
     "filter[collectionyear]": "2025",   # 2023-2024
     "filter[instituteid]": "",  # All institutes
     "list[limit]": 0,   # All records
-
 }
-response = requests.post(DATA_SOURCE, data=payload)
-response.raise_for_status()
 
-soup = BeautifulSoup(response.content, "html.parser")
+for year in range(2020, 2026):
+    payload["filter[collectionyear]"] = str(year)
 
-data = []
-soup_data = soup.find_all('div', class_='stats-item')
-for item in soup_data:
-    institution = item  .find('div', class_='stats-item-title').text.strip()
-    institution = re.search("^ΙΔΡΥΜΑ ::", institution)
-    
-    established = item.find('span', class_='stats-item-date').text.strip()
-    established = re.search("Ημ/νία Ίδρυσης: ", established)
-    
-    variables = item.find_all('div', class_='stats-item-variable-title')
-    print(f"variables: {variables}")
-    for var in variables:
-        print(var)
-        variable = var.text.strip()
-        if variable == "Απόφοιτοι ΠΠΣ Ιδρύματος":
-            graduates = var.find_next_sibling('div', class_='stats-item-variable-value').text.strip()
-        elif variable == "Εγγεγραμμένοι φοιτητές ΠΠΣ Ιδρύματος":
-            registed_students = var.find_next_sibling('div', class_='stats-item-variable-value').text.strip()
-        elif variable == "Εισαχθέντες φοιτητές ΠΠΣ Ιδρύματος":
-            freshmen = var.find_next_sibling('div', class_='stats-item-variable-value').text.strip()
-        elif variable == "Ενεργοί φοιτητές ΠΠΣ Ιδρύματος":
-            active_students = var.find_next_sibling('div', class_='stats-item-variable-value').text.strip()
+    response = requests.post(DATA_SOURCE, data=payload)
+    response.raise_for_status()
 
-    data.append({
-        'institution': institution.string[institution.end():],
-        'established': established.string[established.end():],
-        "gradutes": graduates,
-        "registed_students": registed_students,
-        "freshmen": freshmen,
-        "active_students": active_students,
-    })
+    soup = BeautifulSoup(response.content, "html.parser")
 
-df = pd.DataFrame(data)
-df.to_csv('hahe.csv', index=False)
-
-
+    scrape_data(soup)
